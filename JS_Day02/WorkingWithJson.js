7f359624376f0280f99621f7221279838984e62d
cybertekschool <github@cybertekschool.com>
2020-02-19 16:02:33 -0500

    day02

--- JS_Day02/WorkingWithJson.js
+++ JS_Day02/WorkingWithJson.js
@@ -4,3 +4,8 @@
  console.log(testData.UserName);
  console.log(testData.PassWord);
  console.log(testData.Browser);
+
+
+ var nestedObjects =  require("../JS_Day02/resources/nestedObjects.json");
+
+ console.log(nestedObjects.Employee1.Name);
